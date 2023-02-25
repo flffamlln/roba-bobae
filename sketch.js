53d91560c352576d10d254ce32941ea674262f1a
Yuri Kim <yuri7kim@gmail.com>
2023-02-25 16:06:30 -0500

    start screen

--- sketch.js
+++ sketch.js
@@ -21,13 +21,59 @@ function draw() {
   
 }
 
-//Useful to have multiple elements on one screen
+// Useful to have multiple elements on one screen
+// Home Screen
 function screen1() {
-  // Home Screen
+  // Menu Box
   noStroke();
   fill('#F1DFEC');
   rectMode(CENTER);
-  rect(width/2, height/2, 500, 600, 15);
+  rect(width/2, height/2, 500, 650, 15);
+
+  // Roba-boba Logo
+  strokeWeight(5);
+  stroke(255);
+  fill('#826464');
+  textSize(64);
+  text('roba-boba', width/2, height/2 - 150);
+  textAlign(CENTER);
+
+  // Play Button Box
+  strokeWeight(10);
+  stroke(255);
+  fill('#EDEAF5');
+  rect(width/2, height/2 - 50, 300, 70, 15);
+
+  // Play Button Text
+  strokeWeight(5);
+  stroke(255);
+  fill(0);
+  textSize(30);
+  text('Play', width/2, height/2 - 40);
+  textAlign(CENTER);
+
+  // Scoreboard Button Box
+  strokeWeight(10);
+  stroke(255);
+  fill('#EDEAF5');
+  rect(width/2, height/2 + 150, 300, 260, 15);
+
+  // Scoreboard Text 
+  strokeWeight(5);
+  stroke(255);
+  fill(0);
+  textSize(30);
+  text('Scoreboard', width/2, height/2 + 70);
+  textAlign(CENTER); 
+  
+  // Scoreboard Background Dark Stripes
+  for(let i = 0; i < 6; i++){
+    if(i % 2 != 0){
+      strokeWeight(0);
+      fill('#DED5F5');
+      rect(width/2, height/2 + 75 + i * 30, 290, 30);
+    }
+  }
 }
 
 function screen2() {
